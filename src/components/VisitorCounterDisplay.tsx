6810365f01cefe27abb8da946cfdb3612cc536c0
anttonimarco <anttonimarco@gmail.com>
2026-03-06 20:56:54 -0300

    Visit

--- src/components/VisitorCounterDisplay.tsx
+++ src/components/VisitorCounterDisplay.tsx
@@ -16,7 +16,7 @@ export default async function VisitorCounterDisplay() {
         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
         <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
       </span>
-      {count || 0} Visitantes
+      {count || 0} Visualizações
     </div>
   )
 }
